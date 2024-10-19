0db48ac446dee3e36a41b3b01dcbb296a8ac7c05
alorslouis <alorslouis@users.noreply.github.com>
2024-10-19 14:13:33 -0400

    wip

--- src/utils/parsers/vehicleListParser.ts
+++ src/utils/parsers/vehicleListParser.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z, type ZodTypeAny } from "zod"
 
 const vehicleListing = z.object({ monitor_id: z.number().nullish(), VIN: z.string().nullish(), user_id: z.number().nullish(), identification: z.string().nullish(), name: z.string().nullish(), lat: z.number().nullish(), lon: z.number().nullish(), odometer: z.number().nullish(), last_complete_dataset_date: z.number().nullish(), speed: z.number().nullish(), locked: z.boolean().nullish(), shift_state: z.string().nullish(), in_service: z.boolean().nullish(), rated_range_km: z.number().nullish(), rated_range: z.any().nullish(), prefers_kilometers: z.boolean().nullish(), range_decimal: z.number().nullish(), car_image_url: z.any().nullish(), car_image_path: z.string().nullish(), autocharge: z.any().nullish(), active_high_priority_service_alert_count: z.number().nullish(), active_medium_priority_service_alert_count: z.number().nullish(), active_other_priority_service_alert_count: z.number().nullish(), active_service_alert_count: z.number().nullish(), status_text: z.string().nullish(), location_text: z.string().nullish(), street_location: z.string().nullish(), mobile_access_enabled: z.boolean().nullish(), mobile_access_date: z.any().nullish(), last_online_date: z.any().nullish(), inside_temperature: z.number().nullish(), outside_temperature: z.number().nullish(), driver_temperature_setting: z.number().nullish(), is_climate_enabled: z.boolean().nullish(), temperature_units: z.string().nullish(), user_access_type: z.string().nullish(), can_obtain_owner_level_access: z.boolean().nullish(), has_access_provisioning_access: z.boolean().nullish(), access_provisioning_data: z.any().nullish(), activity_log_available: z.boolean().nullish(), vehicle_commands_available: z.boolean().nullish(), is_portal_vehicle: z.boolean().nullish(), available_commands: z.array(z.any().nullish()).nullish(), has_service_alert_access: z.boolean().nullish(), has_charging_history_access: z.boolean().nullish(), has_charging_invoice_access: z.boolean().nullish(), in_motion: z.boolean().nullish(), tires: z.object({ front_left_pressure: z.number().nullish(), front_right_pressure: z.number().nullish(), rear_left_pressure: z.number().nullish(), rear_right_pressure: z.number().nullish(), front_recommended_pressure: z.number().nullish(), rear_recommended_pressure: z.number().nullish() }).nullish(), battery_health: z.object({ derived_usable_capacity: z.number().nullish(), derived_rated_range_km: z.number().nullish() }).nullish(), anti_theft_mode: z.boolean().nullish(), sentry_mode_enabled: z.boolean().nullish(), sentry_mode_presently_available: z.boolean().nullish(), sentry_persistence_active: z.any().nullish(), remote_start_enabled: z.boolean().nullish(), charging_state_string: z.string().nullish(), charge_port_latch_string_enum: z.string().nullish(), charge_port_open: z.boolean().nullish(), trunk_value: z.number().nullish(), frunk_value: z.number().nullish() })
 
@@ -50,6 +50,51 @@ export const vehicleListParser = z.object({
 
 export type VehicleSolo = z.infer<typeof vehicleListing>
 
+const vehiclePressuresBase = vehicleListing.pick({ tires: true }).shape.tires.unwrap().unwrap().omit({ rear_recommended_pressure: true, front_recommended_pressure: true })
+
+
+//function deepUnwrap<T extends z.ZodTypeAny>(schema: T): z.infer<T> {
+//	if (schema instanceof z.ZodOptional || schema instanceof z.ZodNullable) {
+//		return deepUnwrap(schema.unwrap());
+//	}
+//
+//	if (schema instanceof z.ZodObject) {
+//		const shape = schema.shape as { [key: string]: z.ZodTypeAny };
+//		const unwrappedShape: { [key: string]: any } = {};
+//
+//		for (const key in shape) {
+//			unwrappedShape[key] = deepUnwrap(shape[key]);
+//		}
+//
+//		return unwrappedShape as z.infer<T>;
+//	}
+//
+//	if (schema instanceof z.ZodArray) {
+//		return [deepUnwrap(schema.element)] as z.infer<T>;
+//	}
+//
+//	if (schema instanceof z.ZodUnion) {
+//		return schema.options.map(deepUnwrap) as z.infer<T>;
+//	}
+//
+//	if (schema instanceof z.ZodIntersection) {
+//		return {
+//			...deepUnwrap(schema._def.left),
+//			...deepUnwrap(schema._def.right),
+//		} as z.infer<T>;
+//	}
+//
+//	return schema.parse({}) as z.infer<T>;
+//}
+//
+//const p = deepUnwrap(vehiclePressuresBase)
+//
+//
+//
+//function parseData<T extends z.ZodTypeAny>(data: unknown, schema: T) {
+//	return schema.parse(data) as z.infer<T>;
+//}
+
 const truncatedSolo = vehicleListing.pick({
 	VIN: true,
 	name: true,
@@ -58,3 +103,4 @@ const truncatedSolo = vehicleListing.pick({
 })
 
 export type TruncatedSolo = z.infer<typeof truncatedSolo>
+
