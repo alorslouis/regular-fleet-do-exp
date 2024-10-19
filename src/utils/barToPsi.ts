7d4765c79fa552bf17ce91574f8abc3d91cdc79e
alorslouis <alorslouis@users.noreply.github.com>
2024-10-19 14:13:40 -0400

    wip

--- src/utils/barToPsi.ts
+++ src/utils/barToPsi.ts
@@ -1,13 +1,26 @@
+import { z } from "zod"
+
 type Bar = number | null | undefined
 
+const numberParser = z.number()
+
 export function transformBarToPsi(bar: Bar) {
-	if (bar) return Math.round(bar * 14.5038);
+	if (bar) {
+		return Math.round(bar * 14.5038);
+	} else {
+		throw new Error("NaN!")
+	}
 }
 
-export function transformMultiBarToPist(pressures: Bar[]) {
+export function transformMultiBarToPSI(pressures: Bar[] | undefined) {
 
-	const length = pressures?.length
+	if (!pressures) {
+		throw new Error("nonono")
+	}
 
-	const y = pressures.map(x => transformBarToPsi(x)).filter(x => !!x)
+	const ins = pressures.map(x => transformBarToPsi(x))
+
+	return ins.reduce((acc, currentVal) => acc + currentVal, 0) / ins?.length
+
 
 }
